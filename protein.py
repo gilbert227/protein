2d0531f3648a1043afa7079cbb11e0f3bfecc97e
Gilbert <ghterbeek@hotmail.com>
2020-01-06 16:09:55 +0100

    test

--- protein.py
+++ protein.py
@@ -1,27 +1,46 @@
 import random
-from copy import copy
 
 algorithm = "HHPHHHPH"
 
-dict = {}
 
-for number, amino in enumerate(algorithm):
+for i in range(1):
+    dict = {}
 
-    if number in (0,1):
-        # set starting conditions
-        dict[number] = [amino, (number, 0)]
-    else:
-        prev_coordinate = dict[number-1][1]
+    for number, amino in enumerate(algorithm):
 
-        options = [(prev_coordinate[0] + 1, prev_coordinate[1]), (prev_coordinate[0] - 1, prev_coordinate[1]),
-        (prev_coordinate[0], prev_coordinate[1] + 1), (prev_coordinate[0], prev_coordinate[1] - 1)]
+        if number in (0,1):
+            # set starting conditions
+            dict[number] = [amino, (number, 0)]
+        else:
+            prev_coordinate = dict[number-1][1]
 
-        for i in dict.values():
-            if i[1] in options:
-                 options.remove(i[1])
+            options = [(prev_coordinate[0] + 1, prev_coordinate[1]), (prev_coordinate[0] - 1, prev_coordinate[1]),
+            (prev_coordinate[0], prev_coordinate[1] + 1), (prev_coordinate[0], prev_coordinate[1] - 1)]
 
-        new_coordinate = random.choice(options)
+            for i in dict.values():
+                if i[1] in options:
+                     options.remove(i[1])
 
-        dict[number] = [amino, new_coordinate]
+            new_coordinate = random.choice(options)
 
-print(dict)
+            dict[number] = [amino, new_coordinate]
+
+    for number in range(len(algorithm)):
+        current_coordinate = dict[number]
+        exact_coordinate = current_coordinate[1]
+
+        if number == 0:
+            coordinates_to_delete = dict[number + 1]
+        elif number == len(algorithm) - 1:
+            coordinates_to_delete = dict[number - 1]
+        else:
+            coordinates_to_delete = (dict[number - 1], dict[number + 1])
+
+        options = [(exact_coordinate[0] + 1, exact_coordinate[1]), (exact_coordinate[0] - 1, exact_coordinate[1]),
+        (exact_coordinate[0], exact_coordinate[1] + 1), (exact_coordinate[0], exact_coordinate[1] - 1)]
+
+        print(current_coordinate)
+        print(coordinates_to_delete)
+        print(options)
+
+    print(dict)
