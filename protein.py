81eaa5805caa23678f57434c2d21ef8ead94569a
Gilbert <ghterbeek@hotmail.com>
2020-01-07 09:36:58 +0100

    dit is een voorbeeld

--- protein.py
+++ protein.py
@@ -29,18 +29,20 @@ for i in range(1):
         current_coordinate = dict[number]
         exact_coordinate = current_coordinate[1]
 
-        if number == 0:
-            coordinates_to_delete = dict[number + 1]
-        elif number == len(algorithm) - 1:
-            coordinates_to_delete = dict[number - 1]
-        else:
-            coordinates_to_delete = (dict[number - 1], dict[number + 1])
+        if current_coordinate[0] == 'H':
+            if number == 0:
+                coordinates_to_delete = dict[number + 1]
+            elif number == len(algorithm) - 1:
+                coordinates_to_delete = dict[number - 1]
+            else:
+                coordinates_to_delete = (dict[number - 1], dict[number + 1])
 
-        options = [(exact_coordinate[0] + 1, exact_coordinate[1]), (exact_coordinate[0] - 1, exact_coordinate[1]),
-        (exact_coordinate[0], exact_coordinate[1] + 1), (exact_coordinate[0], exact_coordinate[1] - 1)]
+            options = [(exact_coordinate[0] + 1, exact_coordinate[1]), (exact_coordinate[0] - 1, exact_coordinate[1]),
+            (exact_coordinate[0], exact_coordinate[1] + 1), (exact_coordinate[0], exact_coordinate[1] - 1)]
 
-        print(current_coordinate)
-        print(coordinates_to_delete)
-        print(options)
+
+        #print(current_coordinate)
+        #print(coordinates_to_delete)
+        #print(options)
 
     print(dict)
