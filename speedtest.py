7a74e9cc9b3b5902f02eaac8d776147645d167cd
Gilbert <ghterbeek@hotmail.com>
2020-01-08 14:50:37 +0100

    minor change

--- speedtest.py
+++ speedtest.py
@@ -2,12 +2,14 @@ import random
 import time
 from protein_class import Protein
 
-iterations = 100000
-protein = Protein("HHPHPHPHPHHHHPHPPPHPPPHPPPPHPPPHPPPHPHHHHPHPHPHPHH")
+iterations = 1
+protein = Protein("HHPHHHPH")
 
+paths = []
 start = time.time()
 for i in range(iterations):
-    protein.generate_random_path()
+    path = protein.generate_random_path()
+    paths.append(path)
 
 end = time.time()
 print(end - start)
