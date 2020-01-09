dffed5346ae8acd65b427c020ecc84c897106bbf
samerd09 <54816413+samerd09@users.noreply.github.com>
2020-01-09 15:24:27 +0100

    Combi package

--- protein_class.py
+++ protein_class.py
@@ -1,5 +1,6 @@
 import random
 import operator
+import time
 
 class Protein:
     def __init__(self, sequence):
@@ -11,41 +12,71 @@ class Protein:
         path = {}
         h_positions = []
         stability = 0
-        for index, amino in enumerate(self.sequence):
-            if index in (0, 1):
-                # set starting conditions -- exclude rotations of solution
-                next_coordinate = (0, index)
-                path[index] = [amino, next_coordinate]
+        symmetric = True
 
-                if amino == "H":
-                    h_positions.append(next_coordinate)
+        enum_sequence = list(enumerate(self.sequence))
 
-            else:
-                # choose which strategy to take
+        # set starting conditions -- exclude rotations of solution
+        for i in (0,1):
+            path[i] = [enum_sequence[i][1], (0,i)]
+            if enum_sequence[i][1] == "H":
+                h_positions.append((0,i))
+
+            enum_sequence.pop(0)
+
+
+        for index, amino in enum_sequence:
+
+            if symmetric == True:
+
+                 # choose which strategy to take
                 if strategy == 'random':
-                    next_coordinate = self.random_step(path, index)
+                    next_coordinate = self.random_step(path, index, True)
                 elif strategy == 'greedy':
                     if amino == 'H':
-                        next_coordinate = self.greedy_step(path, index, h_positions)
+                        next_coordinate = self.greedy_step(path, index, h_positions, True)
                     else:
-                        next_coordinate = self.random_step(path, index)
+                        next_coordinate = self.random_step(path, index, True)
 
+                # duplicate
                 if not next_coordinate:
                     return None
                 path[index] = [amino, next_coordinate]
 
                 if amino == "H":
-                    stability += self.get_added_stability(path, next_coordinate, h_positions)
+                    stability += self.get_added_stability(path, next_coordinate, h_positions, symmetric)
+                    h_positions.append(next_coordinate)
+
+                # check wether the new coordinate deviated from x=0
+                if next_coordinate[0] != 0:
+                    symmetric = False
+
+            else:
+                # iterate through sequence
+                 # choose which strategy to take
+                if strategy == 'random':
+                    next_coordinate = self.random_step(path, index, True)
+                elif strategy == 'greedy':
+                    if amino == 'H':
+                        next_coordinate = self.greedy_step(path, index, h_positions, False)
+                    else:
+                        next_coordinate = self.random_step(path, index, False)
+                if not next_coordinate:
+                    return None
+                path[index] = [amino, next_coordinate]
+
+                if amino == "H":
+                    stability += self.get_added_stability(path, next_coordinate, h_positions, symmetric)
                     h_positions.append(next_coordinate)
 
         if not return_stability:
             return path
         return (path, stability)
 
-    def random_step(self, path, index):
+    def random_step(self, path, index, symmetric):
         '''returns coordinates for randomly chosen step if possible'''
         prev_coordinate = path[index-1][1]
-        options = self.get_surrounding_coordinates(prev_coordinate)
+        options = self.get_surrounding_coordinates(prev_coordinate, symmetric)
 
         for i in path.values():
             if i[1] in options:
@@ -55,10 +86,10 @@ class Protein:
             return None
         return random.choice(options)
 
-    def greedy_step(self, path, index, h_positions):
+    def greedy_step(self, path, index, h_positions, symmetric):
         '''returns coordinates for greedy algorithm step if possible'''
         prev_coordinate = path[index-1][1]
-        options = self.get_surrounding_coordinates(prev_coordinate)
+        options = self.get_surrounding_coordinates(prev_coordinate, symmetric)
 
         for i in path.values():
             if i[1] in options:
@@ -72,7 +103,7 @@ class Protein:
 
         values = []
         for coordinate in options:
-            points = self.get_added_stability(path, coordinate, h_positions, False)
+            points = self.get_added_stability(path, coordinate, h_positions, symmetric, False)
             values.append((coordinate, points))
 
         greediest = min(map(operator.itemgetter(1), values))
@@ -84,14 +115,14 @@ class Protein:
 
         return random.choice(new_coordinates)
 
-    def get_added_stability(self, path, coordinate, h_positions, random_step=True):
+    def get_added_stability(self, path, coordinate, h_positions, symmetric, random_step=True):
         '''calculate added stability for given step by evaluating surrounding positions'''
 
         # get positions to evaluate
-        positions = self.get_surrounding_coordinates(coordinate)
+        positions = self.get_surrounding_coordinates(coordinate, symmetric)
 
         # remove previous coordinate in path from positions
-        if random_step:
+        if symmetric == False and random_step:
             positions.remove(path[len(path) - 2][1])
 
         added_stability = 0
@@ -101,13 +132,22 @@ class Protein:
         return added_stability
 
 
-    def get_surrounding_coordinates(self, coordinate):
-        return [
-            (coordinate[0] + 1, coordinate[1]),
-            (coordinate[0] - 1, coordinate[1]),
-            (coordinate[0], coordinate[1] + 1),
-            (coordinate[0], coordinate[1] - 1)
+    def get_surrounding_coordinates(self, coordinate, symmetric):
+
+        # options if symmetric, to avoid extra dubplicates
+        if symmetric:
+            return [
+                (coordinate[0] + 1, coordinate[1]),
+                (coordinate[0], coordinate[1] + 1),
             ]
+
+        else:
+            return [
+                (coordinate[0] + 1, coordinate[1]),
+                (coordinate[0] - 1, coordinate[1]),
+                (coordinate[0], coordinate[1] + 1),
+                (coordinate[0], coordinate[1] - 1)
+                ]
 
 
     def determine_directions(self, path):
@@ -129,3 +169,16 @@ class Protein:
                     else:
                         directions.append(1)
         return directions
+
+    def get_best_path(self, iterations, strategy):
+        start_time = time.time()
+
+        best_path = [0,0]
+
+        for i in range(iterations):
+            path = self.generate_path(strategy)
+
+            if path and path[1] <= best_path[1]:
+                best_path = path
+
+        return best_path[1], (time.time() - start_time)
