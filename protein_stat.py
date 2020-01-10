b34ecc3ac66c6a01c2fced0d819ad3db34af1120
Gregor Bolderink <gbolderink@gmail.com>
2020-01-10 12:45:35 +0100

    updated protein_stat.py

--- protein_stat.py
+++ protein_stat.py
@@ -5,30 +5,25 @@ import matplotlib.pyplot as plt
 
 protein = Protein("HHPHHHPH")
 configs = []
-max_duplicates = 100000
+max_separating_duplicates = 10000
 
-max_iter = 100000
-
-def get_separating_iterations():
+def get_separating_duplicates():
     # get number of generated states between non-duplicate states
     
-    separating_iterations = []
-    count = 0
+    separating_duplications = []
     duplicate_count = 0
     
-    for i in range(max_iter):
-        while duplicate_count < max_duplicates:
-            config = protein.generate_random_path(return_stability=False)
-            if config not in configs:
-                configs.append(config)
-                separating_iterations.append(count)
-                count = 0
-            else:
-                count += 1
-                duplicate_count += 1
-        return separating_iterations
-        
+    while duplicate_count < max_separating_duplicates:
+        config = protein.generate_path("random", return_stability=False)
+        if config not in configs:
+            configs.append(config)
+            separating_duplications.append(duplicate_count)
+            duplicate_count = 0
+        else:
+            duplicate_count += 1
+    return separating_duplications
+    
 
 if __name__ == "__main__":
-    plt.plot(get_separating_iterations())
+    plt.plot(get_separating_duplicates())
     plt.show()
