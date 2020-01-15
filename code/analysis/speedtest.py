05c181730bca4fccf2ba0dd2dc17dc47f74df7c1
Gilbert <ghterbeek@hotmail.com>
2020-01-15 13:49:56 +0100

    speedtest finished, works only in python console, use from classes.speedtest import *

--- code/analysis/speedtest.py
+++ code/analysis/speedtest.py
@@ -1,33 +1,30 @@
-import sys
-
-sys.path.append('../')
-
-
-from code.helpers.navigator import get_step_options, get_added_stability
-
+from classes.protein import Protein
+from algorithms.greedy_path import generate_greedy_path
+from algorithms.random_path import generate_random_path
 from random import choice
 import time
 import operator
-from protein_class import Protein
 
 protein_string = input("What is the Protein string: ")
 iterations_input = int(input("How many iterations (integer): "))
-strategy = input("Which strategy (greedy or random): ")
+strategy = input("Which strategy (g for greedy, r for random): ")
 minimum_stability = int(input("What is the minimum stability (give in negative values): "))
 
 iterations = iterations_input
 protein = Protein(protein_string)
 
-
-
 start = time.time()
 counter = 0
 for i in range(iterations):
-    path = protein.generate_path(strategy)
-    if path != None and path[1] < minimum_stability:
-        print(path)
-        counter += 1
+    if strategy == "g":
+        generate_greedy_path(protein)
+    else:
+        generate_random_path(protein)
 
+    if protein.stability < minimum_stability:
+        print(protein)
+
+        counter += 1
 
 end = time.time()
 print(counter)
