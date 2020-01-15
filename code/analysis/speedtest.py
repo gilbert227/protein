8961ec90f5808d525cd74e5f99280dddd60391b3
Gilbert <ghterbeek@hotmail.com>
2020-01-15 13:09:38 +0100

    speedtest

--- code/analysis/speedtest.py
+++ code/analysis/speedtest.py
@@ -1,7 +1,9 @@
+import sys
+
+sys.path.append('../')
+
+
 from code.helpers.navigator import get_step_options, get_added_stability
-from code.algorithms.greedy_path import generate_greedy_path
-from code.algorithms.random_path import generate_random_path
-from code.classes.protein import Protein
 
 from random import choice
 import time
