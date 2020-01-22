e120f4229c273d93a81e5c9e47c2dc28a5275343
Gregor Bolderink <gbolderink@gmail.com>
2020-01-23 00:53:33 +0100

    partially implemented care (path quality) in forward search

--- code/algorithms/forward_search.py
+++ code/algorithms/forward_search.py
@@ -7,21 +7,26 @@ from code.helpers.navigator import get_step_options
 from copy import deepcopy
 from random import choice
 
-def forward_search(protein, depth=5):
+def forward_search(protein, depth=5, care=0):
     protein.initialize_path()
     while len(protein.path) < len(protein.sequence):
         paths = []
-        look_ahead(protein, depth, paths)
-        best_score = min([temp_protein.stability for temp_protein in paths])
-        quickest = min([len(temp_protein.path) for temp_protein in paths if temp_protein.stability == best_score])
-        protein = deepcopy(choice([temp_protein for temp_protein in paths if temp_protein.stability == best_score and len(temp_protein.path) == quickest]))
+        look_ahead(protein, depth, paths, care)
+        try:
+            # catch exception if path remains empty after look_ahead
+            best_score = min([temp_protein.path_quality for temp_protein in paths])
+            quickest = min([len(temp_protein.path) for temp_protein in paths if temp_protein.path_quality == best_score])
+            protein = deepcopy(choice([temp_protein for temp_protein in paths if temp_protein.path_quality == best_score and len(temp_protein.path) == quickest]))
+        except:
+            # try again
+            return forward_search(protein, depth, care)
     return protein
 
-def look_ahead(protein, depth, paths):
+def look_ahead(protein, depth, paths, care):
     for option in get_step_options(protein):
         temp_protein = deepcopy(protein)
-        temp_protein.add_step(temp_protein.sequence[len(temp_protein.path)], option)
+        temp_protein.add_step(temp_protein.sequence[len(temp_protein.path)], option, care)
         paths.append(temp_protein)
         if depth > 0 and len(temp_protein.path) < len(temp_protein.sequence):
-            look_ahead(temp_protein, depth - 1, paths)
+            look_ahead(temp_protein, depth - 1, paths, care)
 
--- code/analysis/stats.py
+++ code/analysis/stats.py
@@ -25,13 +25,15 @@ amino_colors = {
     'C': 'green'
 }
 
-def generate_path(protein, strategy, greed=1, care=0, chunk_size = 6, chunk_iterations = 500, step_strategy = "random"):
+def generate_path(protein, strategy, greed=1, care=0, chunk_size = 6, chunk_iterations = 500, step_strategy = "random", depth = 3):
     if strategy == "random":
         generate_random_path(protein)
     elif strategy == "greedy":
         generate_greedy_path(protein, greed, care)
     elif strategy == "chunky path":
         generate_chunky_path(protein, chunk_size, chunk_iterations, step_strategy, care)
+    elif strategy == "forward search":
+        return forward_search(protein, depth)
 
 def get_next_unique_config(protein, strategy, configs=[], max_iterations=10000, greed=1, care=0, chunk_size = 6, chunk_iterations = 500, step_strategy = "random"):
     ''' returns first configuration not in configs '''
@@ -42,7 +44,7 @@ def get_next_unique_config(protein, strategy, configs=[], max_iterations=10000, 
             return (i, config, True)
     return (None, None, False)
 
-def get_separating_duplicates(protein, strategy, duplication_threshold, greed=1, care=0, chunk_size = 6, chunk_iterations = 500, step_strategy = "random"):
+def get_separating_duplicates(protein, strategy, duplication_threshold, greed=1, care=0, chunk_size=6, chunk_iterations=500, step_strategy="random", depth=3):
     ''' get number of duplicates generated between found unique states '''
     configs = []
     separating_duplicates = []
@@ -60,12 +62,23 @@ def get_separating_duplicates(protein, strategy, duplication_threshold, greed=1,
 
     return separating_duplicates, len(separating_duplicates)
 
-def get_best_config(protein, strategy, iterations, greed=1, care=0, chunk_size = 6, chunk_iterations = 500, step_strategy = "random"):
+def get_best_config(protein, strategy, iterations, greed=1, care=0.2, chunk_size=6, chunk_iterations=500, step_strategy="greedy", depth=3):
     best_stability = 0
     best_config = None
+    
+    if strategy == 'forward search':
+        # temporary fix for forward search: TODO: fix this issue
+        for i in range(iterations):
+            new_protein = generate_path(protein, 'forward search', iterations, depth=depth)
+            stability = new_protein.stability
+            print(stability)
+            if stability < best_stability:
+                best_stability = stability
+                best_config = deepcopy(new_protein)
+        return best_config
 
     for i in range(iterations):
-        generate_path(protein, strategy, greed, care, chunk_size, chunk_iterations, step_strategy)
+        generate_path(protein, strategy, greed, care, chunk_size, chunk_iterations, step_strategy, depth)
         stability = protein.stability
         if stability < best_stability:
             best_stability = stability
@@ -73,7 +86,7 @@ def get_best_config(protein, strategy, iterations, greed=1, care=0, chunk_size =
 
     return best_config
 
-def get_stability_histogram(protein, strategy, iterations, greed=1, care=0, chunk_size = 6, chunk_iterations = 500, step_strategy = "random"):
+def get_stability_histogram(protein, strategy, iterations, greed=1, care=0, chunk_size = 6, chunk_iterations = 500, step_strategy = "random", depth=3):
     stabilities = []
 
     for i in range(iterations):
