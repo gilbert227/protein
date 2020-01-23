612b6d127b3f590cc5f4db0c5d035db3d208b584
Gilbert <ghterbeek@hotmail.com>
2020-01-23 16:31:08 +0100

    minor bug fix

--- code/analysis/speedtest.py
+++ code/analysis/speedtest.py
@@ -2,7 +2,7 @@ from code.algorithms.greedy_path import generate_greedy_path
 from code.algorithms.random_path import generate_random_path
 from code.algorithms.chunky_path import generate_chunky_path
 from code.algorithms.forward_search import forward_search
-from code.algorithms.stats import *
+from code.analysis.stats import *
 import time
 
 def speedtest(protein, strategy, minutes = 1, greed = 1, care = 0, chunk_size = 6, chunk_iterations = 100, step_strategy = "g"):
