40ef1cd8a01529eab891967677b1b54c65c71191
Gregor Bolderink <gbolderink@gmail.com>
2020-01-24 14:55:21 +0100

    adjusted greed and care functions

--- code/helpers/navigator.py
+++ code/helpers/navigator.py
@@ -3,7 +3,7 @@
 
     supporting functions for navigating proteins and surroundings
 '''
-
+from numpy.random import choice
 
 def get_surrounding_coordinates(protein, coordinate):
     ''' returns positions surrounding inserted coordinate depending on the third dimension'''
@@ -65,9 +65,45 @@ def get_added_stability(protein, amino, step, care=0):
             if position in amino_positions:
                 binding_score, blocking_penalty = protein.bond_stabilities[amino][amino_neighbor]
                 added_stability += binding_score
-                weight += binding_score + blocking_penalty**(care)
+                weight += (1 - care) * binding_score + care * (blocking_penalty ** care)
     return added_stability, weight
 
+
+def make_up_your_mind(protein, amino, greed, care):
+    ''' choose next step for protein using the greed function '''
+
+    options = get_step_options(protein)
+    
+    # check for dead ends
+    if options == []:
+        return None
+
+    added_stabilities = []
+    weights = []
+    for option in options:
+        added_stability, weight = get_added_stability(protein, amino, option, care)
+        added_stabilities.append(added_stability)
+        weights.append(weight)
+
+    # generate probability distribution based on the greed-function
+    # bias is used to shift weights to generate meaningful probability distribution
+    bias = 1
+
+    # invert and shift values to non-negative numbers
+    max_weight = max(weights)
+    weights = [abs(weight - max_weight) for weight in weights]
+    best_score = max(weights)
+
+    # apply greed function
+    weights = [greed * (weight == best_score) * (weight + bias) + (1 - greed) * (weight + ((1 - greed) * bias)) for weight in weights]
+
+    # normalize weights
+    sum_weights = sum(weights)
+    weights = [weight / sum_weights for weight in weights]
+
+    # choose number representing the index for chosen option
+    i = choice([i for i in range(len(options))], 1, p=weights)[0]
+    return options[i], added_stabilities[i], weights[i]
 
 def get_path_directions(protein):
     '''
@@ -78,6 +114,7 @@ def get_path_directions(protein):
        -3, 3 represent unit steps along the z-axis if protein in 3D
        0 terminates the sequence
     '''
+    
     # obtain positions from path
     positions = [point[1] for point in protein.path]
 
